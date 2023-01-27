5158f781651ada654add974158458fe54a939ce3
Haniket <haniket2812001@gmail.com>
2023-01-27 15:22:36 +0530

    commit

--- client/src/app/url.service.ts
+++ client/src/app/url.service.ts
@@ -3,26 +3,27 @@ import {HttpClient} from '@angular/common/http'
 import { Observable, throwError } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
 import { UrlCount,LinkedUrl,DomainUrl } from './urlClasses';
+import { environment } from 'src/environments/environment';
 @Injectable({
   providedIn: 'root'
 })
 export class UrlService {
-  private baseUrl = 'http://localhost:3000/';
+  private baseUrl = environment.apiUrl;
   constructor(private http: HttpClient) { }
 
   getUrlCount(domain:any): Observable<UrlCount[]>{
-    return this.http.get<UrlCount[]>(this.baseUrl+"getUrlCount?domain="+domain);
+    return this.http.get<UrlCount[]>(this.baseUrl+'getUrlCount?domain='+domain);
   }
 
   getLinkedUrl(url:any): Observable<LinkedUrl[]>{
-    return this.http.get<LinkedUrl[]>(this.baseUrl+"getLinkedUrls?url="+url)
+    return this.http.get<LinkedUrl[]>(this.baseUrl+'getLinkedUrls?url='+url)
   }
 
   getDomain(): Observable<DomainUrl[]>{
-    return this.http.get<DomainUrl[]>(this.baseUrl+"getAllDomain")
+    return this.http.get<DomainUrl[]>(this.baseUrl+'getAllDomain')
   }
 
   resetMap(): Observable<any>{
-    return this.http.get(this.baseUrl+"resetMap");
+    return this.http.get(this.baseUrl+'resetMap');
   }
 }
--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://lucky-hare-attire.cyclic.app'
+  apiUrl: 'https://lucky-hare-attire.cyclic.app/'
 };
