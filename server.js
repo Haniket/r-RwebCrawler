473d6470401e307b5f4b7dcb0f874823db8c96de
Haniket <haniket2812001@gmail.com>
2023-01-27 15:07:03 +0530

    commit

--- server.js
+++ server.js
@@ -3,22 +3,26 @@ const express = require('express');
 require('dotenv').config();
 const crawlerService = require('./services/crawler.service');
 const app = express();
-const allowedOrigins=['https://lucky-hare-attire.cyclic.app/','http://localhost:3000','http://localhost:4200']
-app.use(cors({
-  origin: function (origin, callback) {
-    // allow requests with no origin
-    // (like mobile apps or curl requests)
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      var msg =
-        "The CORS policy for this site does not " +
-        "allow access from the specified Origin.";
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
-  credentials: true,
-}));
+// const allowedOrigins=['https://lucky-hare-attire.cyclic.app/','http://localhost:3000','http://localhost:4200']
+// app.use(cors({
+//   origin: function (origin, callback) {
+//     // allow requests with no origin
+//     // (like mobile apps or curl requests)
+//     if (!origin) return callback(null, true);
+//     if (allowedOrigins.indexOf(origin) === -1) {
+//       var msg =
+//         "The CORS policy for this site does not " +
+//         "allow access from the specified Origin.";
+//       return callback(new Error(msg), false);
+//     }
+//     return callback(null, true);
+//   },
+//   credentials: true,
+// }));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
 
 app.use(express.json({ extended: false }));
 const path = require('path');
